9657dfb92357d2706bfd0d2b34d2266feba0a5e1
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-02-20 17:35:44 +0200

    Remove string extensions for conversion to GUID

--- ProcessingTools.Extensions.Text/ProcessingTools.Extensions.Text/StringExtensions.cs
+++ ProcessingTools.Extensions.Text/ProcessingTools.Extensions.Text/StringExtensions.cs
@@ -184,56 +184,6 @@ namespace ProcessingTools.Extensions.Text
         }
 
         /// <summary>
-        /// Parse string to <see cref="Guid"/> or generates new <see cref="Guid"/> if parse is invalid.
-        /// </summary>
-        /// <param name="source">Source string to be parsed.</param>
-        /// <returns>Parsed or new <see cref="Guid"/>.</returns>
-        public static Guid ToNewGuid(this string source)
-        {
-            if (!string.IsNullOrWhiteSpace(source) && Guid.TryParse(source, out Guid result))
-            {
-                return result;
-            }
-
-            return Guid.NewGuid();
-        }
-
-        /// <summary>
-        /// Parse object to <see cref="Guid"/> or generates new <see cref="Guid"/> if parse is invalid.
-        /// </summary>
-        /// <param name="source">Source object to be parsed.</param>
-        /// <returns>Parsed or new <see cref="Guid"/>.</returns>
-        public static Guid ToNewGuid(this object source)
-        {
-            return (source?.ToString()).ToNewGuid();
-        }
-
-        /// <summary>
-        /// Parse string to <see cref="Guid"/> or returns empty <see cref="Guid"/> if parse is invalid.
-        /// </summary>
-        /// <param name="source">Source string to be parsed.</param>
-        /// <returns>Parsed or empty <see cref="Guid"/>.</returns>
-        public static Guid ToEmptyGuid(this string source)
-        {
-            if (!string.IsNullOrWhiteSpace(source) && Guid.TryParse(source, out Guid result))
-            {
-                return result;
-            }
-
-            return Guid.Empty;
-        }
-
-        /// <summary>
-        /// Parse object to <see cref="Guid"/> or returns empty <see cref="Guid"/> if parse is invalid.
-        /// </summary>
-        /// <param name="source">Source object to be parsed.</param>
-        /// <returns>Parsed or empty <see cref="Guid"/>.</returns>
-        public static Guid ToEmptyGuid(this object source)
-        {
-            return (source?.ToString()).ToEmptyGuid();
-        }
-
-        /// <summary>
         /// Extracts a string from between a pair of delimiters. Only the first instance is found.
         /// </summary>
         /// <param name="source">Input String to work on.</param>
